abd4caed809e7677d27d0304c77153c1ac695aaa
fauez <fauez.passos@inpe.br>
2020-01-21 11:03:45 -0300

    send mail to recovery password with mailTrap

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const crypto = require('crypto')
+const Mail = use('Mail')
 const User = use('App/Models/User')
 
 class ForgotPasswordController {
@@ -13,6 +14,21 @@ class ForgotPasswordController {
       user.token_created_at = new Date()
 
       await user.save()
+
+      await Mail.send(
+        ['emails.forgot_password'],
+        {
+          email,
+          token: user.token,
+          link: `${request.input('redirect_url')}?token=${user.token}`
+        },
+        message => {
+          message
+            .to(user.email)
+            .from('staff@gonode.io', 'Staff | GoNode')
+            .subject('Password Recovery')
+        }
+      )
     } catch (err) {
       return response.status(err.status).send({ error: { message: 'Something wrong, email exists?' } })
     }
