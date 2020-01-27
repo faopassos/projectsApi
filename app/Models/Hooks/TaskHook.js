6902184b735066a3097e2f26fed606ec25fd3dc4
fauez <fauez.passos@inpe.br>
2020-01-27 11:32:14 -0300

    queue with redis container

--- app/Models/Hooks/TaskHook.js
+++ app/Models/Hooks/TaskHook.js
@@ -1,7 +1,7 @@
 'use strict'
 
-const Mail = use('Mail')
-const Helpers = use('Helpers')
+const Kue = use('Kue')
+const Job = use('App/Jobs/NewTaskMail')
 
 const TaskHook = exports = module.exports = {}
 
@@ -14,20 +14,8 @@ TaskHook.sendNewTaskMail = async taskInstance => {
 
   const { title } = taskInstance
 
-  await Mail.send(
-    ['emails.new_task'],
-    { username, title, hasAttachment: !!file },
-    message => {
-      message
-        .to(email)
-        .from('fauez.passos@outlook.com', 'Fauez | faotech')
-        .subject('Nova tarefa')
-
-      if (file) {
-        message.attach(Helpers.tmpPath(`uploads/${file.file}`), {
-          filename: file.name
-        })
-      }
-    }
+  Kue.dispatch(Job.key,
+    { email, username, file, title },
+    { attempts: 3 }
   )
 }
